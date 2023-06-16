8ada9d155240852b0aad3b906deb961b74302d16
Paul <wmdev0808@gmail.com>
2023-06-15 19:46:02 -0700

    -- Add an example for $object::class which can be used instead of get_class in PHP 8

---
+++ challenge-003/05.php
@@ -0,0 +1,48 @@
+<?php
+
+class Conversation {}
+
+$obj = new Conversation();
+
+// switch (get_class($obj)) {
+//   case 'Conversation':
+//     $type = 'started_conversation';
+//     break;
+  
+//   case 'Reply':
+//     $type = 'replied_to_conversation';
+//     break;
+
+//   case 'Comment':
+//     $type = 'commented_on_lesson';
+//     break;
+// }
+
+// switch ($obj::class) {
+//   case 'Conversation':
+//     $type = 'started_conversation';
+//     break;
+  
+//   case 'Reply':
+//     $type = 'replied_to_conversation';
+//     break;
+
+//   case 'Comment':
+//     $type = 'commented_on_lesson';
+//     break;
+// }
+
+
+// $type = match (get_class($obj)) {
+//     'Conversation' => 'started_conversation',
+//     'Reply'=> 'replied_to_conversation',
+//     'Comment' => 'commented_on_lesson'
+// };
+
+$type = match ($obj::class) {
+    'Conversation' => 'started_conversation',
+    'Reply'=> 'replied_to_conversation',
+    'Comment' => 'commented_on_lesson'
+};
+
+echo $type;
